21914a95111c9d3068ac0ff627258e28049304fd
Andrey Maslov <andreymaslov94@gmail.com>
2023-03-02 06:00:07 +0200

    set portraitUp orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
 import 'services/payment_plans_service.dart';
 import 'stacked/stacked_app.locator.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   await setupLocator();
   await locator<PaymentPlansService>().init();
 
